867c9813cd24efd7bbb2f2b209ab15ac652d2237
Alexandr Litvinov <70410264+SNK-LT@users.noreply.github.com>
2023-06-26 14:15:50 +0300

    Recreated all the project. Now it starts

---
+++ src/main/java/com/example/SoftVersionControl/SoftVersionControlApplication.java
@@ -0,0 +1,12 @@
+package com.example.SoftVersionControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftVersionControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftVersionControlApplication.class, args);
+	}
+}
---
+++ src/main/java/com/example/SoftVersionControl/models/User.java
@@ -0,0 +1,9 @@
+package com.example.SoftVersionControl.models;
+
+public class User {
+    private Long id;
+    private String email;
+    private String password;
+    private String name;
+    private boolean active;
+}
