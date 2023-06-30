b337f37d2d43153225aa2a092d5810147f11c16b
Alexandr Litvinov <70410264+SNK-LT@users.noreply.github.com>
2023-06-30 12:04:57 +0300

    Changed table name to users, so it now connects to tthe normal table, that existed. And some local fixes

--- src/main/java/com/example/SoftVersionControl/domain/User.java
+++ src/main/java/com/example/SoftVersionControl/domain/User.java
@@ -1,14 +1,12 @@
 package com.example.SoftVersionControl.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-@Entity // This tells Hibernate to make a table out of this class
+@Entity// This tells Hibernate to make a table out of this class
+@Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
 
     private String login;
